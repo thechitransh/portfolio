4c8e714e90001c7d36eda2de957ab72679b4677f
Chitransh <thechitranshsrivastava@gmail.com>
2023-09-21 11:32:17 +0530

    testyfy

--- app/Resume/page.js
+++ app/Resume/page.js
@@ -1,10 +1,26 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import img from "@/public/res.png"
 import style from "./page.module.css"
 import icon from "@/public/icon.svg"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function page() {
+  const notify = () =>{
+    toast.success('Downloaded', {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  }
   return (
     <div>
         <Image
@@ -20,8 +36,10 @@ function page() {
         src={icon}
         width={60}
         height={60}
+        onClick={notify}
         />
         </a>
+        <ToastContainer/>
         </div>
     </div>
   )
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
